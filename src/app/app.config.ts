000fa0ce18998f84aedf91b1669d02d9c2af4931
Mike Helfrich <helfrichmichael@users.noreply.github.com>
2025-07-03 22:19:42 -0400

    Additional improvements and fixed the lack of QR code modification. Printing also now works!

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,12 +8,12 @@ export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimationsAsync()]
 };
 
-export const HOST_IP = 'localhost';
+export const HOST_IP = '10.0.0.20';
 
 export const HOST_PORT = 11180;
 
 export const API_URL = `http://${HOST_IP}:${HOST_PORT}/api/v1/`;
 
-export const DESIGN_NAME = 'test';
+export const DESIGN_NAME = 'MC_Label';
 
-//vhttp://localhost:11180/api/v1/print?design=test&variables={"PLANT_NAME":"<b>$PLANT_NAME</b>"}&printer=System-\\Windows-10-vm\tsc ttp-247&window=show&copies=1
+export const PRINTER_ID = 'TSC-USB-T4524231356';
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,47 +1,42 @@
 import { Component } from '@angular/core';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatButtonModule} from '@angular/material/button';
-import {FormGroup, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import { API_URL, DESIGN_NAME } from '../app.config';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatButtonModule } from '@angular/material/button';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { API_URL, DESIGN_NAME, PRINTER_ID } from '../app.config';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [MatGridListModule, ReactiveFormsModule, MatSelectModule, MatInputModule,MatFormFieldModule, HttpClientModule, MatButtonModule],
+  imports: [MatGridListModule, ReactiveFormsModule, MatSelectModule, MatInputModule, MatFormFieldModule, HttpClientModule, MatButtonModule,],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
   plantLabelForm = new FormGroup({
-    copies: new FormControl(0, [Validators.required, Validators.pattern('^[0-9]*$')]),
-    plantName: new FormControl(`Monstera Deliciosa 'Thai Constellation'`,[Validators.required]),
-    url: new FormControl('https://mikescarnivores.com',[Validators.required]),
+    copies: new FormControl(1, [Validators.required, Validators.pattern('^[0-9]*$')]),
+    plantName: new FormControl(`Monstera Deliciosa 'Thai Constellation'`, [Validators.required]),
+    url: new FormControl('https://mikescarnivores.com', [Validators.required]),
   });
 
   get previewImage() {
-    console.info('Rendering');
-    return API_URL + `print?design=${DESIGN_NAME}&variables=%7B%22PLANT_NAME%22%3A%22%3Cb%3E${this.plantLabelForm.get('plantName')?.value}%3C%2Fb%3E%22%7D`
+    const plantName = `${this.plantLabelForm.get('plantName')?.value}`;
+    const url = this.plantLabelForm.get('url')?.value
+    return API_URL + `print?design=MC_Label&variables=%7B%22PLANT_NAME%22%3A%22%3Cb%3E${plantName}%3C%2Fb%3E%22%2C%22URL%22%3A%22${url}%22%7D`
   }
 
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) { }
 
   printLabel() {
-    const request = {
-      'design': DESIGN_NAME,
-      'variables': {
-        'PLANT_NAME': `<b>${this.plantLabelForm.get('plantName')?.value}</b>`,
-        'URL': `${this.plantLabelForm.get('url')?.value}`
-      },
-      'printer': 'Microsoft Print to PDF',
-      'window': 'show',
-      'copies': this.plantLabelForm.get('copies')?.value,
-    }
-    this.http.post(`print?design=${DESIGN_NAME}`, request).subscribe(result => {
+    const copies = this.plantLabelForm.get('copies')?.value;
+    const plantName = `<b>${this.plantLabelForm.get('plantName')?.value}</b>`;
+    const url = this.plantLabelForm.get('url')?.value
+    this.http.post(`http://10.0.0.20:11180/api/v1/print?design=${DESIGN_NAME}&variables={"PLANT_NAME":"${plantName}","URL":"${url}"}&printer=${PRINTER_ID}&window=show&copies=${copies}`, {}).subscribe(result => {
       console.info('Response from Label.live local API: ', result);
+
     })
   }
 }
